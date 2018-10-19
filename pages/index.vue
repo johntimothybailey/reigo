31e2a4633ecceed271c9d293ce7027a2990a27c5
BobbyMBailey <RobertBailey7@gmail.com>
2018-10-18 21:06:47 -0500

    Created the base UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,44 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
+  <v-layout row justify-center align-center>
+    <v-flex xs12>
       <v-card>
-        <v-card-title class="headline">REIgo</v-card-title>
         <v-card-text>
           <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field prefix="$"
+              v-model.number="arv"
+              label="Full Market Value" required
+            />
+            <v-text-field prefix="$"
+              v-model="purchasePrice"
+              label="Purchase Price"  
+            />
             <v-text-field
-              v-model="arv"
-              label="ARV" required
+              v-model="address"
+              label="Address"
+            />
+             <v-text-field
+              v-model="year"
+              label="Year Built"  
+            />
+            <v-text-field
+              v-model="sqft"
+              label="Square Footage"  
             />
           </v-form>
         </v-card-text>
         <v-card-actions>
+          <v-card-title class="headline">
+            <v-rating
+              v-model="rating"
+              readonly
+              half-increments
+              background-color="orange lighten-3"
+              color="orange"
+              medium
+            ></v-rating>
+          </v-card-title>
           <v-spacer></v-spacer>
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+          <v-btn color="primary" flat nuxt to="/inspire">Run the numbers</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -21,17 +46,23 @@
 </template>
 
 <script>
-import {VForm, VTextField} from 'vuetify'
+import {VForm, VTextField, VRating} from 'vuetify'
 export default {
   name: 'Home',
   components: {
     VForm,
-    VTextField
+    VTextField,
+    VRating
   },
   data: () => {
     return {
       valid: true,
-      arv: 0
+      arv: null,
+      address: null,
+      sqft: null,
+      year: null,
+      purchasePrice: null,
+      rating: 0
     }
   }
 }
